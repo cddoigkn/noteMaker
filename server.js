2a9bdb4e87562d069bcd3f689d1fe0cb89ec9123
Casey Doig <cddoigkn@gmail.com>
2023-11-01 17:02:26 -0500

    the bases getting covered again

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
+/* 
+  application needs an import for db.json file using fs module
+  HTML ROUTES
+  create GET /notes html route for returning the notes.html file 
+  create GET * html route for returning the index.html file 
+  API ROUTES
+  GET /api/notes reads db.json
+  then returns all saved notes as JSON
+  POST /api/notes receives new note from ui
+  then writes note to db.json
+  then returns the new note to the ui
+*/
+
 const express = require('express');
+const fs = require("fs");
 
 const path = require('path');
 
@@ -10,15 +24,6 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
-
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
-
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
-);
-
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
